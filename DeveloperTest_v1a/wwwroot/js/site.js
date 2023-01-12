8202e505ae5ded6c97b0c544ad530bc568f9bc32
Rabbee Fazley <Rabbee.Fazley@SOLOPROTECT.COM>
2023-01-12 13:47:07 +0000

    added filtering by specific columns but broek number of default rows

--- DeveloperTest_v1a/wwwroot/js/site.js
+++ DeveloperTest_v1a/wwwroot/js/site.js
@@ -1,8 +1,20 @@
 ﻿$(document).ready(function () {
-    $('#myTable').DataTable({
-        "scrollY": "450px",
-        "scrollCollapse": true,
-        "paging": true, 
-        "lengthMenu": [20,40,60,80,100]
-    });
+	// Setup - add a text input to each footer cell
+	$('#myTable tfoot th').each(function () {
+
+		var column = this;
+		var title = $('#myTable thead th').eq($(this).index()).text();
+		$(this).html('<input type="text" placeholder="Search ' + title + '" />');
+	});
+
+	// DataTable
+	var table = $('#myTable').DataTable();
+
+	// Apply the filter
+	$("#myTable tfoot input").on('keyup change', function () {
+		table
+			.column($(this).parent().index() + ':visible')
+			.search(this.value)
+			.draw();
+	});
 });
