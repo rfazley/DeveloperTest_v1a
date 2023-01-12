0b17fd1ff88531d72da051f88ae64daaa9102642
Rabbee Fazley <Rabbee.Fazley@SOLOPROTECT.COM>
2023-01-12 13:59:18 +0000

    fixed default number of records plus listing all records

--- DeveloperTest_v1a/wwwroot/js/site.js
+++ DeveloperTest_v1a/wwwroot/js/site.js
@@ -1,4 +1,15 @@
 ﻿$(document).ready(function () {
+
+	$('#myTable').dataTable({
+		"lengthMenu":
+					[
+						[20, 40, 60, 80, -1],
+						[20, 40, 60, 80, 'All']
+					],
+		"pageLength": 20
+	});
+
+
 	// Setup - add a text input to each footer cell
 	$('#myTable tfoot th').each(function () {
 
@@ -9,6 +20,8 @@
 
 	// DataTable
 	var table = $('#myTable').DataTable();
+
+	
 
 	// Apply the filter
 	$("#myTable tfoot input").on('keyup change', function () {
